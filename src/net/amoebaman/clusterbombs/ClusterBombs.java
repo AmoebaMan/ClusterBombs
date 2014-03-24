12ba689081dbee8135380b9db6881ad8f6a4ff5b
Dennison Richter <drichter@amoebaman.net>
2014-03-24 10:22:59 -0400

    Comments!

--- src/net/amoebaman/clusterbombs/ClusterBombs.java
+++ src/net/amoebaman/clusterbombs/ClusterBombs.java
@@ -16,10 +16,25 @@ public class ClusterBombs extends JavaPlugin implements Listener{
 	
 	@EventHandler
 	public void onEntityExplode(EntityExplodeEvent event){
+		/*
+		 * Only interested in creeper explosions
+		 */
 		if(event.getEntityType() == EntityType.CREEPER)
+			/*
+			 * A random number of times...
+			 */
 			for(int i = 0; i < (Math.random() * 3) + 3; i++){
+				/*
+				 * Spawn a new creeper
+				 */
 				Entity creeper = event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), EntityType.CREEPER);
+				/*
+				 * Set their velocity so they "pop" out
+				 */
 				creeper.setVelocity(new Vector((Math.random() - 0.5) * 0.5, Math.random() * 0.5, (Math.random() - 0.5) * 0.5));
+				/*
+				 * If they're SUPER unlucky, power up the creeper
+				 */
 				if(Math.random() < 0.05)
 					((Creeper) creeper).setPowered(true);
 			}
